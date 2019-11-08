c2472a9c0db9ae976ec2542c64e98d23a44581d9
jmjles <jmjles@gmail.com>
2019-11-07 16:58:49 -0800

    mvp complete 2

--- src/components/apod.js
+++ src/components/apod.js
@@ -1,4 +1,7 @@
 import React,{useState, useEffect} from 'react';
+import Font from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
 const axios = require('axios');
 const dates = ['2019-11-06', '2019-11-04', '2019-11-03', '2019-11-02'];
 
@@ -18,6 +21,7 @@ function Apod() {
             setData(prev => ([...prev,{
                 title:res.data.title,
                 url:res.data.hdurl,
+                date: res.data.date,
                 copyright:res.data.copyright,
                 explanation:res.data.explanation
             }]));
@@ -27,14 +31,24 @@ function Apod() {
         <section>
         {
             data.map(item =>{
+
                 const style = {
-                    backgroundImage:`url('${item.url}')`
+                    background:`url('${item.url}') no-repeat center center fixed`
                 }
+
+                const paperStyle = {
+                    background:'rgba(0, 0, 0, 0.25)'
+                }
+                
                 return (
-                    <div key={item.title} className="Entry" style={style}>
-                        <h1>{item.title}</h1>
-                        <p>{item.explanation}</p>
-                        <p>{item.copyright}</p>
+                    <div key={item.title} className="Entry">
+                        <img src={item.url} className="imgBackground"/>
+                        <Paper style={paperStyle}>
+                            <Font variant="h1">{item.title}</Font>
+                            <Font variant="subtitle1">{item.date}</Font>
+                            <Font variant="body1">{item.explanation}</Font>
+                            <Font variant="body1">{item.copyright}</Font>
+                        </Paper>
                     </div>
                 )
             })
